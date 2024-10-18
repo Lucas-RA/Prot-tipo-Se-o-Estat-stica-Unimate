7965f0e803508a6df12965e640e9055ca316ec8a
Lucas-RA <150272932+Lucas-RA@users.noreply.github.com>
2024-10-18 00:00:57 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+
+        button.classList.toggle('active');
+
+        if (button.classList.contains('active')) {
+            accordionContent.style.display = 'block';
+        } else {
+            accordionContent.style.display = 'none';
+        }
+
+        // Fechar outros accordions quando um é aberto
+        document.querySelectorAll('.accordion-content').forEach(content => {
+            if (content !== accordionContent) {
+                content.style.display = 'none';
+                content.previousElementSibling.classList.remove('active');
+            }
+        });
+    });
+});
+
+
